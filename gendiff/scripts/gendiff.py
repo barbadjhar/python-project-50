940e3a14d73cb8f6d096f9759d3382e33d7b3354
Sergey Krash <badj.xy@gmail.com>
2025-07-05 14:41:19 +0300

    update: в help добавлен флаг -f

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,9 @@ def main():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
+    
+    parser.add_argument('-f', '--format', help='set format of output')
+    
     args = parser.parse_args()
     print(args)
 
